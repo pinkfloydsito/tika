005d7e156b5ee9ad9819073aa47787bd96b3af74
Sebastian <ascacere92@gmail.com>
2020-05-28 01:27:32 -0500

    Updates

---
+++ precache-manifest.78c742b0633e8351108b7e95b0cf3362.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad40e8ee87a2e9e890b604cb30ea4777",
+    "url": "/tika/index.html"
+  },
+  {
+    "revision": "5f2369a0f1ee5db6a56a",
+    "url": "/tika/static/css/main.dd0069db.chunk.css"
+  },
+  {
+    "revision": "585c107a9cd75a46e1cc",
+    "url": "/tika/static/js/2.eadd0fde.chunk.js"
+  },
+  {
+    "revision": "4cca3d5ae541b398de3251f137340a61",
+    "url": "/tika/static/js/2.eadd0fde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f2369a0f1ee5db6a56a",
+    "url": "/tika/static/js/main.34176de3.chunk.js"
+  },
+  {
+    "revision": "e6eb7762a2e70e7be693",
+    "url": "/tika/static/js/runtime-main.709d9b9d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tika/static/media/logo.5d5d9eef.svg"
+  }
+]);
